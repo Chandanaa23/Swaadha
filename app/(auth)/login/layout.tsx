bba5599e0b67a82cfda5d967b87a66e81879794c
Chandan23-G <Chandana@dataseedtech.com>
2025-12-11 17:19:26 +0530

    Updated input styles

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,4 +1,10 @@
-export default function AuthLayout({ children }) {
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function AuthLayout({ children }: Props) {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       {children}
